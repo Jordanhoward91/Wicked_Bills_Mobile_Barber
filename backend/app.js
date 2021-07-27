0c6d065ad0a7814600ad689500647b8476483aef
Jordan Howard <jordanhoward1991@gmail.com>
2021-07-26 20:29:15 -0600

    added styles for header

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,14 @@
-const express = require ('express');
+const express = require('express');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://Max:KLeoWx0ycoFH87rz@cluster0.qld5u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin","*");
@@ -13,6 +21,17 @@ app.use((req, res, next) => {
     "GET, POST, PATCH, DELETE, OPTIONS"
   );
   next();
+});
+
+app. post("/api/posts", (req, res, next) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added successfully'
+  });
 });
 
 app.use('/api/posts', (req, res, next) => {
